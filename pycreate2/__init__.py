202e8a12fd57a07c328e129aa33f3335dc003a2c
Kevin <walchko@users.noreply.github.com>
2017-05-26 23:26:43 -0600

    Update __init__.py

--- pycreate2/__init__.py
+++ pycreate2/__init__.py
@@ -1,5 +1,8 @@
 from .create2api import Create2
-from . import OI
+# from . import OI
 from .create2api import Fatal, Error, Warning
 
 from .version import __version__
+
+__license__ = 'MIT'
+__author__ = 'Kevin Walchko'
